89fba91ffeb635fb1e866b5b51c452d4d74124d7
Icaro Ferreira <icaro.iky@gmail.com>
2021-09-30 10:30:44 -0300

    Add Interactivity on the BoxCard

--- src/components/card-box/CardBox.jsx
+++ src/components/card-box/CardBox.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function CardBox() {
+function CardBox({ bodyDisplay = false }) {
+  const [show, setShow] = useState(bodyDisplay);
+  
   return(
     <div className="bg-yellow-100">
-      <header className="flex space-x-4 bg-yellow-400">
+      <header
+        className="flex space-x-4 bg-yellow-400"
+        onClick={() => setShow(!show)}
+      >
         <h2>Nome do Card</h2>
         <div>blabla</div>
       </header>
-      <main>
-        <div>Imagem</div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, temporibus. Debitis, magnam obcaecati! Perspiciatis ex, natus corporis consectetur iusto provident autem odio reprehenderit temporibus quas, hic qui culpa sapiente cumque?</p>
-        <ul>
-          <li>JavaScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>React</li>
-        </ul>
-      </main>
+      {
+        show
+        && (
+          <main>
+            <div>Imagem</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, temporibus. Debitis, magnam obcaecati! Perspiciatis ex, natus corporis consectetur iusto provident autem odio reprehenderit temporibus quas, hic qui culpa sapiente cumque?</p>
+            <ul>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>React</li>
+            </ul>
+          </main>
+        )
+      }
     </div>
   )
 }
