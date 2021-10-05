6189b41bf5342d7502d9d41eca1f81013aab6ec9
Icaro Ferreira <icaro.iky@gmail.com>
2021-10-05 10:33:53 -0300

    Desktop adjustment

--- src/components/contact-box/ContactBox.jsx
+++ src/components/contact-box/ContactBox.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Modal } from '..';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 import '../components.css';
 
@@ -11,21 +13,24 @@ function ContactBox() {
   }
 
   return (
-    <div className="shdw grid justify-items-center rounded bg-yellow-100 m-2 mb-16 p-2 mx-auto w-1/6">
+    <div className="shdw grid justify-items-center rounded bg-yellow-100
+      m-2 mb-16 p-2 mx-auto w-10/12 md:w-3/12">
       <div className="flex space-x-4 text-lg underline">
         <a
           href="https://www.linkedin.com/in/icaro-ferreira-gomes-b99681110/"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Linkedin
+          <FontAwesomeIcon icon={ faLinkedin }/>
+          <span className="p-1">Linkedin</span>
         </a>
         <a
           href="https://github.com/icarofgomes"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Github
+          <FontAwesomeIcon icon={ faGithub }/>
+          <span className="p-1">Github</span>
         </a>
       </div>
       <div className="">
--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import './modal.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Modal({ handleClick }) {
   return (
-    <div class="modal text-center">
-      <div class="bg-yellow-100 m-auto w-2/5 h-48">
-        <div className="flex flex-col text-center my-6">
+    <div className="modal text-center">
+      <div className="bg-yellow-100 m-auto w-10/12 h-48 md:w-3/12">
+        <h2 className="font-bold text-lg pt-1">
+          Contatos
+        </h2>
+        <div className="flex flex-col text-center my-1">
           <a
+            className="p-2"
             href="https://api.whatsapp.com/send?phone=5511970772734"
             target="_blank"
             rel="noreferrer noopener"
           >
-            119970772734
+            <FontAwesomeIcon icon={ faWhatsapp } />
+            <span className="p-1">11 9970772734</span>
           </a>
           <a
+            className="p-2"
             href="mailto:icaro.iky@gmail.com"
             target="_blank"
             rel="noreferrer noopener"
           >
-            icaro.iky@gmail.com
+            <FontAwesomeIcon icon={ faEnvelope } />
+            <span className="p-1">icaro.iky@gmail.com</span>
           </a>
         </div>
         <button
